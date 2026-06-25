7ea035c858c8a2d22bf0f90ad7cd9142708de408
Mark Jacobsen <mark@markjacobsen.net>
2026-06-24 22:26:52 -0400

    signal group.sh more robust

--- scripts/signal/group.sh
+++ scripts/signal/group.sh
@@ -16,6 +16,18 @@ if [[ -z "$MSG" ]]; then
     read -rp "Enter the message you would like to send: " MSG
 fi
 
+# If TO doesn't end with "=", try to match by group name (case insensitive)
+if [[ "$TO" != *= ]]; then
+    MATCHED_ID=$(curl -s -X POST http://127.0.0.1:8080/api/v1/rpc \
+        -H "Content-Type: application/json" \
+        -d '{"jsonrpc":"2.0","method":"listGroups","id":1,"params":{}}' \
+        | jq -r --arg to "${TO,,}" '.result[] | select(.name | ascii_downcase == $to) | .id' | head -1)
+    
+    if [[ -n "$MATCHED_ID" ]]; then
+        TO="$MATCHED_ID"
+    fi
+fi
+
 curl -s -X POST http://127.0.0.1:8080/api/v1/rpc \
   -H "Content-Type: application/json" \
   -d "{
